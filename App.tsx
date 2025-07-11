e66a149b0fd17588a9c84bed5190690d24ed90be
gabrielmaxgb <gmaxgomes@gmail.com>
2025-07-10 22:31:09 -0300

    Adds styled components

--- App.tsx
+++ App.tsx
@@ -8,13 +8,31 @@
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: papayawhip;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  color: palevioletred;
+  font-weight: bold;
+`;
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <NewAppScreen templateFileName="App.tsx" /> */}
+        <Container>
+          <Title>AgendaEdu Todo App</Title>
+      </Container>
     </View>
   );
 }
