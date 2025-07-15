98c7523f60922a44a13cb2b6ff1eee7440b4fb8b
gabrielmaxgb <gmaxgomes@gmail.com>
2025-07-15 04:49:23 -0300

    - Adds Sentry

--- App.tsx
+++ App.tsx
@@ -6,26 +6,41 @@ import { theme } from './src/theme/theme';
 import Toast from 'react-native-toast-message';
 import { Toast as CustomToast } from './src/components/common/customToasts/CustomToasts';
 import type { BaseToastProps } from 'react-native-toast-message';
+import * as Sentry from '@sentry/react-native';
+// import Button from './src/components/common/button/Button';
 
-const successToast = (props: BaseToastProps) => <CustomToast {...props} variant="success" />;
-const errorToast = (props: BaseToastProps) => <CustomToast {...props} variant="error" />;
-const infoToast = (props: BaseToastProps) => <CustomToast {...props} variant="info" />;
+Sentry.init({
+  dsn: 'https://dfc1a10ade18e4d9ee18930c09180406@o4509671371440128.ingest.us.sentry.io/4509671379304448',
+});
+
+Sentry.captureException(new Error('Erro de teste!'));
+
+const successToast = (props: BaseToastProps) => (
+  <CustomToast {...props} variant="success" />
+);
+const errorToast = (props: BaseToastProps) => (
+  <CustomToast {...props} variant="error" />
+);
+const infoToast = (props: BaseToastProps) => (
+  <CustomToast {...props} variant="info" />
+);
 
 const queryClient = new QueryClient();
 
-export default function App() {
+export default Sentry.wrap(function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <AppRoutes />
+        {/* <Button title='Try Sentry' onPress={ () => { Sentry.captureException(new Error('New error')) }}/> */}
         <Toast
           config={{
             success: successToast,
             error: errorToast,
             info: infoToast,
           }}
-          />
+        />
       </ThemeProvider>
     </QueryClientProvider>
   );
-}
+});
